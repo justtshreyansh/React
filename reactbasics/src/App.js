44d295ac38bb049ce71aef1a675fdd787f23d004
Shreyansh <shreyanshmarshal2@gmail.com>
2024-01-01 19:20:20 +0530

    Learn about how to run and optimize react by vitereact and normal react

---
+++ reactbasics/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Chai aur React | Shreyansh </h1>
+  );
+}
+
+export default App;
---
+++ vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur React via viteReact</h1>
+  )
+}
+
+export default App
